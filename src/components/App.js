44191542b3a5b37495b015e8fe8fdb11b808737b
balashovmb <balashovmb@gmail.com>
2020-08-21 16:48:22 +0500

    Base functional

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import BookContainer from './Book/BookContainer';
+import Header from './common/Header'
+import Footer from './common/Footer';
+import ToTheTopButton from './common/ToTheTopButton'
+import {book} from '../data/data'
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <h3>Подписаться на книгу</h3>
+        <BookContainer book = {book}/>
+        <Footer />
+        <ToTheTopButton/>
+      </>
+    )
+  }
+}
+
+export default App;
